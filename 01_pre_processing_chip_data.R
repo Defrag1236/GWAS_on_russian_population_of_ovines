59607c352117218a0793a5928392841e77730453
zlobin <defrag12@gmail.com>
2019-10-03 14:50:53 +0700

    continue pre-processing

--- 01_pre_processing_chip_data.R
+++ 01_pre_processing_chip_data.R
@@ -10,3 +10,27 @@ chip_data <- fread ("F1_OAM_RMNV.txt", head=T, stringsAsFactors=F)
 # parse the full table for separate tables of samples
 
 chip_data_list <- split(chip_data, rep(1:48, each=606006))
+
+# rename matrix in list  
+
+names_of_samples <- c(1:48)
+
+for (n in (1:48)) {
+
+	number_of_sample <- as.numeric(unlist(strsplit(as.matrix(chip_data_list[[n]][1,1]), "No"))[2])
+
+	names_of_samples[n] <- paste ("sample", number_of_sample, sep="_")
+
+	}
+
+names(chip_data_list) <- names_of_samples
+
+# delete unused info
+
+for ( n in (1:48)) {
+
+	chip_data_list [[n]] <- chip_data_list[[n]] [,c("SNP.Name", "Chr", "Position", "SNP")] 
+	
+
+
+	}
