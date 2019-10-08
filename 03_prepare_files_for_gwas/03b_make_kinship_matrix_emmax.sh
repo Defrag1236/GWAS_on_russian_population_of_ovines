00b82bd36d3b142f151b690e7b3e66f0a660108c
zlobin <defrag12@gmail.com>
2019-10-08 22:58:54 +0700

    make kinship matrix on filtered data

--- 03_prepare_files_for_gwas/03b_make_kinship_matrix_emmax.sh
+++ 03_prepare_files_for_gwas/03b_make_kinship_matrix_emmax.sh
@@ -4,8 +4,8 @@ cd /home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/da
 
 # make tped/tfam files 
 
-plink --bfile for_all_samples --recode12 --output-missing-genotype 0 --transpose --out for_all_samples
+plink --bfile for_all_samples --recode12 --output-missing-genotype 0 --transpose --out for_all_samples_filtered --maf 0.1 --geno 0.05
 
 # make kinship IBS matrix 
 
-/home/common/projects/ovine_selection/Soft/emmax/emmax-kin-intel64 -v -d 10 /home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/data/plink_files/for_all_samples
+/home/common/projects/ovine_selection/Soft/emmax/emmax-kin-intel64 -v -d 10 /home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/data/plink_files/for_all_samples_filtered
