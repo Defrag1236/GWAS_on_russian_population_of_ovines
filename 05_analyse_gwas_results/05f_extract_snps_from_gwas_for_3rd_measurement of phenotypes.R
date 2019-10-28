531c6dae7ba8c0927ca637abe9759a8c7d92bbc3
zlobin <defrag12@gmail.com>
2019-10-28 18:17:31 +0700

    Do after GWAS analysis

---
+++ 05_analyse_gwas_results/05f_extract_snps_from_gwas_for_3rd_measurement of phenotypes.R
@@ -0,0 +1,39 @@
+### extract snps from gwas for 3rd measurement of phenotypes ###
+
+# load data
+
+library(data.table)
+
+setwd("/home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/results/gemma_results/3m/output")
+
+trait_list <- list()
+
+for (n in (1:11)) {
+
+	file_name <- paste ("3m_", n, "_trait.assoc.txt", sep="")
+
+	trait_list[[n]] <- fread(file_name, head=T, stringsAsFactors=F)
+
+	}
+
+setwd("/home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/data/")
+
+pheno_3m_key <- fread("pheno_3m_key.txt", head=F, stringsAsFactors=F)
+
+
+# extract significant snps from each trait 
+
+trait_list_snps <- list()
+
+for (n in (1:11)) {
+
+	trait_list_snps[[n]] <-  subset(trait_list[[n]], trait_list[[n]]$p_wald<0.05/(33*nrow(trait_list[[n]])), select=1:12)
+
+	}
+
+
+# save results
+
+setwd("/home/common/projects/ovine_selection/GWAS_on_russian_population_of_ovines/results/gemma_results")
+
+save(trait_list_snps, file="significant_snps_for_3m_measurement_of_phenotypes.Rdata")
